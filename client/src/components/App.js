7f91e766522f23b59fb66b727ef8967f7fdc2a86
harrydry <harrydry1996@gmail.com>
2017-09-29 22:37:26 +0100

    ss

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,6 +9,7 @@ class App extends Component {
 		this.state = {
 			top: 0,
 			left: 0,
+			isLoading: undefined,
 		}
 	}
 
@@ -17,6 +18,16 @@ class App extends Component {
 			top: e.pageY - window.scrollY - 2,
 			left: e.pageX - 12,
 		})
+	}
+
+	imgLoaded(e){
+		this.setState({isLoading: true})
+		console.log('e')
+	}
+
+	vidLoaded(e){
+		this.setState({isLoading: false})
+		console.log('jeeee')
 	}
 
 	render() {
@@ -33,15 +44,18 @@ class App extends Component {
 		const body = {
 			cursor: `${cursorOn}`
 		}
+		const {isLoading} = this.state
 		return (
 			<div onMouseMove={(e) => this.mousePosition(e)} style={body}>
 				<div className="cursor" style={cursor} />
 				<div className="wrapper">
 					<EditableTweet />
 					<PaymentBlock />
-					<div className="laptopPlayback">
-						<img src="https://s3.eu-west-2.amazonaws.com/lifeishappening/macBook.png" className="macBook"/>
-						<video src="https://s3.eu-west-2.amazonaws.com/lifeishappening/obama.mp4" autoPlay muted playsInline loop width="400" className="obamaVideo" />
+					<div className={`laptopPlayback ${isLoading ? 'isLoading' : ''}`}>
+						<img src="https://s3.eu-west-2.amazonaws.com/lifeishappening/macBook1.png" onLoad={(e) => this.imgLoaded(e)} className="macBook"/>
+						<video src="https://s3.eu-west-2.amazonaws.com/lifeishappening/obama.mp4" onCanPlayThrough={(e) => this.vidLoaded(e)} autoPlay muted playsInline loop width="400" className="obamaVideo" />
+						<div className="lds-css ng-scope positionSpinner">
+						<div className="lds-spinner"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div></div>
 					</div>
 				</div>
 			</div>
