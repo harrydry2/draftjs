649454365617f72b28533f048a9e354960c28b72
harrydry <harrydry1996@gmail.com>
2017-11-10 22:07:02 +0000

    s

--- client/src/components/mainPage/Page3/editableTweet/Profile.js
+++ client/src/components/mainPage/Page3/editableTweet/Profile.js
@@ -8,7 +8,7 @@ class Profile extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			username: 'Edit This Username And Press Enter ✂️ ',
+			username: '@140_Canvas',
 			popover: true,
 			i: 0,
 			favImgCounter: 0,
