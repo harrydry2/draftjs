bee13a52b0c5466967580aa65b3722f0e69db9d9
harrydry <harrydry1996@gmail.com>
2017-11-18 17:48:25 +0000

    ss

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,9 @@
 module.exports = {
-	mongoURI: "mongodb://harrydry:DavidLuiz4@ds147964.mlab.com:47964/draftjs",
-	stripePublishableKey: 'pk_test_9gCdD2DfM1XFS6Tdo6mtL4zq',
-	stripeSecretKey: 'sk_test_m7Lwx7pzergqBw553u6pJsLD',
-	mailHost: "smtp.postmarkapp.com",
-	mailPort: "2525",
-	mailUser: "6f06e0b0-b866-4aba-a469-01cd5d7494d2",
-	mailPass: "6f06e0b0-b866-4aba-a469-01cd5d7494d2",
-}
+  mongoURI: 'mongodb://harrydry:DavidLuiz4@ds147964.mlab.com:47964/draftjs',
+  stripePublishableKey: 'pk_live_IYA9l3NvFPJTE7PIIxLq1b4o',
+  stripeSecretKey: 'sk_live_djEAkYUTfjnJpFiCBISUuFyR',
+  mailHost: 'smtp.postmarkapp.com',
+  mailPort: '2525',
+  mailUser: '6f06e0b0-b866-4aba-a469-01cd5d7494d2',
+  mailPass: '6f06e0b0-b866-4aba-a469-01cd5d7494d2',
+};
